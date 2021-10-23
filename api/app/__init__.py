0d0d9d1f27638f9b5f8deafff509b8c258d06a90
vanceb <vance@axxe.co.uk>
2021-10-23 16:24:59 +0100

    Placed job checking and retreival under a /job/<id> URL and added simple job logging

--- api/app/__init__.py
+++ api/app/__init__.py
@@ -4,9 +4,25 @@ from config import Config
 from redis import Redis
 from rq import Queue
 
+import logging
+from logging.handlers import RotatingFileHandler
+import os
+
 app = Flask(__name__)
 app.config.from_object(Config)
 
 q = Queue(connection=Redis(host="redis", port="6379"))
 
 from app import routes
+
+# Logging
+if not os.path.exists('/data/logs'):
+    os.makedirs('/data/logs')
+file_handler = RotatingFileHandler('/data/logs/map2laser.log',
+                                    maxBytes=1024000,
+                                    backupCount=10)
+file_handler.setFormatter(logging.Formatter(
+        '%(asctime)s %(levelname)s: %(message)s'))
+file_handler.setLevel(logging.INFO)
+app.logger.addHandler(file_handler)
+app.logger.setLevel(logging.INFO)
--- api/app/routes.py
+++ api/app/routes.py
@@ -19,10 +19,16 @@ def index():
             return jsonify(result), 400
 
         result = q.enqueue("job.run_job", app.config["MAP_CONFIG"], job_def)
+        app.logger.info(str(result.id) + " => " + str(job_def))
         return result.id
 
+@app.route('/log', methods=['GET'])
+def show_log():
+    with open('/data/logs/map2laser.log', 'r') as f:
+        l = f.read()
+    return str(l), 200, {'Content-Type': 'text/plain'}
 
-@app.route('/<id>', methods=['GET'])
+@app.route('/job/<id>', methods=['GET'])
 def get_result(id):
     try:
         mapjob = Job.fetch(id, connection=Redis(host="redis", port="6379"))
@@ -30,6 +36,7 @@ def get_result(id):
         return abort(404) 
 
     if mapjob.is_finished:
+        app.logger.info(str(id) + " =>  Completed")
         return ET.tostring(mapjob.result), 200, {'Content-Type': 'image/svg+xml'}
 
     elif mapjob.is_queued:
@@ -37,6 +44,7 @@ def get_result(id):
     elif mapjob.is_started:
         result = {"Status": "Started"}
     elif mapjob.is_failed:
+        app.logger.info(str(id) + " =>  Failed")
         result = {"Status": "Failed - Maybe timed out, please select a smaller area or fewer features"}
     elif mapjob.is_deferred:
         result = {"Status": "Job deferred"}
